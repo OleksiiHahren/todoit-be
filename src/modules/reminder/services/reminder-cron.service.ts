4be89b76e28953f821746e1fe66aafa8ae81ad59
OleksiiHahren <olexiygagren@gmail.com>
2022-11-05 21:18:19 +0200

    reminder added service methods

---
+++ src/modules/reminder/services/reminder-cron.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+
+@Injectable()
+export class ReminderCronService {
+
+  @Cron('0 0 * * *')
+  dailyReminder() {
+
+  }
+
+  @Cron('0 0 * * 0')
+  remindAboutSkippedTasks() {
+  }
+}
--- src/modules/reminder/services/reminder.service.ts
+++ src/modules/reminder/services/reminder.service.ts
@@ -1,5 +1,20 @@
-import { Resolver } from '@nestjs/graphql';
-import { ReminderDto } from '@root/modules/reminder/dto/reminder.dto';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+import { EmailReminderRepository } from '@root/modules/email-sending/repository/email-reminder-repository';
 
-@Resolver(() => ReminderDto)
-export class ReminderService {}
+@Injectable()
+export class ReminderService {
+  private readonly logger = new Logger(ReminderService.name);
+  constructor(private reminderRepo: EmailReminderRepository) {
+  }
+  @Cron('0 0 * * *')
+  dailyReminder() {
+    const targetItems = this.reminderRepo.getAllForRemind();
+
+  }
+
+  @Cron('0 0 * * 0')
+  remindAboutSkippedTasks() {
+    return this.reminderRepo.getAllSkippedTasks()
+  }
+}
